d1dc2329f2b37a9fedd41e605e6fcbb59da6139b
Joran Angevaare <jorana@nikhef.nl>
2021-08-13 16:31:35 +0200

    I don't know bash on windows

--- .github/scripts/install_on_windows.sh
+++
@@ -1,5 +0,0 @@
-pip install emcee
-pip install git+https://github.com/jorana/wimprates
-git clone https://github.com/jorana/verne
-pip install -e verne
-pip install git+https://github.com/JohannesBuchner/PyMultiNest
