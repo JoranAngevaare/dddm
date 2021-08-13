b7edc266131c302afb10f96cd793c21d34737fd4
Joran Angevaare <jorana@nikhef.nl>
2021-08-13 17:02:54 +0200

    too few points

--- tests/test_nestle_shielded.py
+++ tests/test_nestle_shielded.py
@@ -1,16 +1,14 @@
 import logging
-
 import DirectDmTargets as dddm
 
 log = logging.getLogger()
 
 
 def test_nested_simple_nestle_earth_shielding():
-
     fit_class = dddm.NestedSamplerStatModel('Xe')
     fit_class.config['sampler'] = 'nestle'
     fit_class.config['tol'] = 0.1
-    fit_class.config['nlive'] = 3
+    fit_class.config['nlive'] = 10
     fit_class.config['max_iter'] = 1
     fit_class.config['earth_shielding'] = True
     fit_class.config['save_intermediate'] = True
