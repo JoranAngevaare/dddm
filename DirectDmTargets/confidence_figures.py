9e4e8bf7c61b5181990c52642b1d591c51dbdd0c
Joran angevaare <jorana@nikhef.nl>
2021-08-25 09:49:49 +0000

    Automated autopep8 fixes

--- DirectDmTargets/confidence_figures.py
+++ DirectDmTargets/confidence_figures.py
@@ -58,7 +58,7 @@ class DDDMResult:
                           self.result_summary()],
                          axis=1)
 
-    def get_from_config(self, to_get: str, if_not_available = None):
+    def get_from_config(self, to_get: str, if_not_available=None):
         return self.result.get('config', {}).get(to_get, if_not_available)
 
     def get_samples(self):
