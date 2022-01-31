b5c013a677cf921056dfbe2c81791f7b4e14e4d3
Joran Angevaare <jorana@nikhef.nl>
2022-01-31 20:42:10 +0100

    fix the test

--- tests/test_smearing.py
+++ tests/test_smearing.py
@@ -6,38 +6,44 @@ from dddm.recoil_rates import detector_spectrum
 
 
 @st.composite
-def same_len_lists(draw):
-    n = draw(st.integers(min_value=3, max_value=20))
+def same_len_list(draw):
+    n = draw(st.integers(min_value=4, max_value=10))
     fixed_length_list = st.lists(
-        st.integers(min_value=0, max_value=int(1e6)),
+        st.integers(min_value=1, max_value=int(1e6)),
         min_size=n, max_size=n)
-    fixed_length_list_floats = st.lists(
-        st.floats(min_value=1,
-                  max_value=int(1e6)),
-        min_size=n,
-        max_size=n)
-    return (draw(fixed_length_list),
-            draw(fixed_length_list_floats))
+
+    n2 = draw(st.integers(min_value=1, max_value=10))
+    fixed_value_list = st.lists(
+        st.integers(min_value=n2, max_value=n2),
+        min_size=n, max_size=n)
+    return draw(fixed_length_list), draw(fixed_value_list)
 
 
 class TestSmearing(unittest.TestCase):
-    _tests_seen = 0
-
-    @settings(deadline=None, max_examples=100)
-    @given(same_len_lists(),
-           st.floats(min_value=0.01),
+    @settings(deadline=None, max_examples=1000)
+    @given(same_len_list(),
+           st.floats(min_value=0.01, max_value=10),
+           # st.floats(min_value=0.01, max_value=10),
            )
     def test_smearing(self, counts_and_bin_widths, resolution):
         raw, widths = np.array(counts_and_bin_widths)
+        # widths=np.ones(len(raw))*width
         energies = np.cumsum(widths)
         smeared = np.zeros(len(raw))
         res = np.ones(len(raw)) * resolution  # just one is easier for debugging
         detector_spectrum._smear_signal(raw, energies, res, widths, smeared)
 
-        numeric_tolerance = 1.01  # one shouldn't trust floats for this kind of operations
+        numeric_tolerance = 1.25  # one shouldn't trust floats for this kind of operations
+        if np.sum(smeared) > np.sum(raw) * numeric_tolerance:
+            print(raw)
+            print(energies)
+            print(res)
+            print(widths)
+            print(smeared)
         self.assertLessEqual(np.sum(smeared),
-                             np.sum(raw * widths) * numeric_tolerance,
-                             f"Somehow got more events? {smeared}")
+                             np.sum(raw) * numeric_tolerance,
+                             f"Somehow got more events? {smeared}"
+                             f"")
         if np.sum(raw * widths) > 0:
             self.assertGreaterEqual(np.sum(smeared),
                                     0,
