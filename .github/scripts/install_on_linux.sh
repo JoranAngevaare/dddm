cf9adf39dc8118a4bd8325d889aaad9165c87709
Joran R. Angevaare <jorana@nikhef.nl>
2022-05-28 14:45:21 +0200

    Update install_on_linux.sh

--- .github/scripts/install_on_linux.sh
+++ .github/scripts/install_on_linux.sh
@@ -23,4 +23,6 @@ echo "Doing other conda and pip:"
 # We need the data files from this
 git clone https://github.com/joranangevaare/verne ../verne
 pip install -e ../verne
+git clone https://github.com/JoranAngevaare/DarkELF
+pip install -e DarkELF
 echo "done"
